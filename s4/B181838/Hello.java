60c2e70151829642a4e0ef9130cd4984a8521e26
Gakumaru <g181838@xdev05.edu.tut.ac.jp>
2020-12-01 17:40:11 +0900

    Add something

--- s4/B181838/Hello.java
+++ s4/B181838/Hello.java
@@ -4,6 +4,7 @@ import java.lang.*;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello..(B181838)");
+	System.out.println("goodmorning. ohayou");
     }
 }
 
